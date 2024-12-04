8dc63d376d62cd6ec6b9046b5ced8f2599b6559c
Yama Roni <yamaroni8@gmail.com>
2024-12-04 20:06:28 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // Ini file Javascript
+
+function validateForm() {
+    let inputBeratBadan = document.getElementById('berat-badan-input').value;
+    let inputUsia = document.getElementById('usia-input').value;
+    let inputTinggiBadan = document.getElementById('tinggi-badan-input').value;
+
+    console.log('BB:' + inputBeratBadan);
+    console.log('Usia:' + inputUsia);
+    console.log('Tinggi:' + inputTinggiBadan);
+    if (inputBeratBadan != '' && inputUsia != '' && inputTinggiBadan != '') {
+        let calculate = parseInt(inputBeratBadan) + parseInt(inputTinggiBadan);
+        updateResult(calculate);
+    } else {
+        alert('Inputan Anda Kosong! Mohon Cek Kembali');
+    }
+}
+
+function updateResult(value) {
+    document.getElementById('result-calculation').innerHTML = value;
+}
